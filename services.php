229292eebc11cc651e557dcf83631b365a768744
Melo254 <owuormalvine75l@gmail.com>
2024-07-27 13:50:11 +0300

    canged

---
+++ services.php
@@ -0,0 +1,495 @@
+<?php include 'php/actions.php'; ?>
+<!doctype html>
+<html class="no-js" lang="zxx">
+<head>
+<!-- Meta Tags -->
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="keywords" content="Site keywords here">
+<meta name="description" content="">
+<meta name='copyright' content=''>
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+<!-- Title -->
+<title>armely</title>
+
+<!-- Favicon -->
+<link rel="icon" href="img/logo/logo1.png">
+
+<!-- Google Fonts -->
+<link href="https://fonts.googleapis.com/css?family=Poppins:200i,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i&display=swap" rel="stylesheet">
+
+<!-- Bootstrap CSS -->
+<link rel="stylesheet" href="css/bootstrap.min.css">
+<!-- Nice Select CSS -->
+<link rel="stylesheet" href="css/nice-select.css">
+<!-- Font Awesome CSS -->
+<link rel="stylesheet" href="css/font-awesome.min.css">
+<!-- icofont CSS -->
+<link rel="stylesheet" href="css/icofont.css">
+<!-- Slicknav -->
+<link rel="stylesheet" href="css/slicknav.min.css">
+<!-- Owl Carousel CSS -->
+<link rel="stylesheet" href="css/owl-carousel.css">
+<!-- Datepicker CSS -->
+<link rel="stylesheet" href="css/datepicker.css">
+<!-- Animate CSS -->
+<link rel="stylesheet" href="css/animate.min.css">
+<!-- Magnific Popup CSS -->
+<link rel="stylesheet" href="css/magnific-popup.css">
+
+<!-- Medipro CSS -->
+<link rel="stylesheet" href="css/normalize.css">
+<link rel="stylesheet" href="style.css">
+<link rel="stylesheet" href="css/responsive.css">
+<link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+
+
+
+</head>
+<body>
+
+
+
+<section>   
+<!-- Floating Action Button -->
+   <div class="floating-btn">
+    <button id="myBtn"  style="border-radius: 50%; height: 60px; width: 60px; background-color: rgb(47,85,151);"  type="button" class="btn btn-primary btn-lg h1">
+      <i class="fa fa-comments "></i>
+    </button>
+  </div>
+<div id="myModal" class="modal-chat">
+
+  <!-- Modal content -->
+  <div class="modal-content-chat col-lg-4">
+    <span class="close">&times;</span>
+    <iframe src="https://copilotstudio.microsoft.com/environments/Default-588cadf4-9902-4465-86c0-8bcf04f4f102/bots/crc65_armelyCom/webchat?__version__=2"
+    frameborder="0" style="width: 100%; height: 80%;"></iframe>  
+  </div>
+
+</div>
+</section>
+
+<!-- Header Area -->
+<header class="header" >
+<!-- Topbar -->
+<div class="topbar">
+<div class="container">
+<div class="row">
+<div class="col-lg-6 col-md-5 col-12">
+      <!-- Contact -->
+      <style>
+
+      </style>
+      <ul class="top-link">
+            <li>
+                  
+            </li>
+      
+      </ul>
+      <!-- End Contact -->
+</div>
+<div class="col-lg-6 col-md-7 col-12">
+      <!-- Top Contact -->
+      <ul class="top-contact">
+            <li><i class="fa fa-phone"></i>+1 972 460 0643</li>
+            <li><i class="fa fa-envelope"></i><a href="mailto:info@armely.com">info@armely.com</a></li>
+      </ul>
+      <!-- End Top Contact -->
+</div>
+</div>
+</div>
+</div>
+<!-- End Topbar -->
+<!-- Header Inner -->
+<div class="header-inner">
+<div class="container">
+<div class="inner">
+<div class="row">
+      <div class="col-lg-3 col-md-3 col-12">
+            <!-- Start Logo -->
+            <div class="logo">
+                  <a href="index"><img style="height: 60px;" src="img/logo/logo.svg" alt="#"></a>
+            </div>
+            <!-- End Logo -->
+            <!-- Mobile Nav -->
+            <div class="mobile-nav"></div>
+            <!-- End Mobile Nav -->
+      </div>
+      <div class="col-lg-9 col-md-9 col-12">
+            <!-- Main Menu -->
+            <div class="main-menu">
+                  <nav class="navigation">
+                        <ul class="nav menu">
+                              <li class=""><a href="#">Why Us <i class="icofont-rounded-down"></i></a>
+                                    <ul class="dropdown">
+                                          <li><a href="index.html">Company Overview</a></li>
+                                          <li><a href="index.html">Career Opportunities</a></li>
+                                    </ul>
+                              </li>
+                              <li><a href="#">Services <i class="icofont-rounded-down"></i></a>
+                                    <ul class="dropdown">
+                                          <li><a href="blog-single.html">All Services</a></li>
+                                          <li><a href="blog-single.html">AI Services</a></li>
+                                          <li><a href="blog-single.html">Data Services</a></li>
+                                          <li><a href="blog-single.html">Freemiums</a></li>
+
+                                    </ul>
+                              </li>
+                              <li><a href="#">Industries</a></li>
+                              <li><a href="#">Insights <i class="icofont-rounded-down"></i></a>
+                                    <ul class="dropdown">
+                                          <li><a href="404.html">Blog Articles</a></li>
+                                          <li><a href="404.html">Customer Stories</a></li>
+                                          <li><a href="404.html">Case Studies</a></li>
+                                    </ul>
+                              </li>
+                              <li><a href="contact.html">Contact Us</a></li>
+                              <li><a  >
+                                    <i data-toggle="modal" data-target="#exampleModal" style="display: block !important;" class="fa fa-search p-1"></i>     
+                               </a>
+                              </li>
+                              <li><a  >
+                                    <i style="display: block !important;" class="icofont icofont-moon p-1"></i>   
+                                    <!-- <i style="display: block !important;" class="icofont icofont-sun p-1"></i>      -->
+                               </a>
+                              </li>
+                        </ul>
+                  </nav>
+            </div>
+            <!--/ End Main Menu -->
+      </div>
+      <div class="col-lg-2 col-12">
+            <div class="get-quote">
+                  
+            </div>
+      </div>
+</div>
+</div>
+</div>
+</div>
+<!--/ End Header Inner -->
+</header>
+<!-- End Header Area -->
+
+<!-- Header Area -->
+<!-- Breadcrumbs -->
+<div class="breadcrumbs overlay">
+      <div class="container">
+            <div class="bread-inner">
+                  <div class="row">
+                        <div class="col-12">
+                              <h2>Services</h2>
+                              <ul class="bread-list">
+                                    <li><a href="index.html">Home</a></li>
+                                    <li><i class="icofont-simple-right"></i></li>
+                                    <li class="active">Blog Single</li>
+                              </ul>
+                        </div>
+                  </div>
+            </div>
+      </div>
+</div>
+<!-- End Breadcrumbs -->
+
+
+<!-- Start Schedule Area -->
+
+<!--/End Start schedule Area -->
+
+<!-- Start portfolio -->
+<section class="portfolio section" >
+<div class="container">
+<div class="row">
+<div class="col-lg-12">
+<div class="section-title">
+      <h2>Case Studies</h2>
+      <center><hr class="default-background hr" ></center>
+</div>
+</div>
+</div>
+</div>
+<div class="container-fluid">
+<div class="row">
+<div class="col-lg-12 col-12">
+<div class="owl-carousel portfolio-slider">
+      
+      <?php displayRecentIndustryListings() ?>
+</div>
+</div>
+</div>
+</div>
+</section>
+<!--/ End portfolio -->
+
+
+
+<!-- Pricing Table -->
+<section class="pricing-table section">
+<div class="container">
+<div class="row">
+<div class="col-lg-12">
+<div class="section-title">
+      <h2>See Our Pricing Tiers</h2>
+            <center><hr class="default-background hr" ></center>
+</div>
+</div>
+</div>
+<div class="row">
+<!-- Single Table -->
+<div class="col-lg-4 col-md-12 col-12">
+<div class="single-table">
+      <!-- Table Head -->
+      <div class="table-head">
+            <div class="icon">
+                  <i class="icofont icofont-server"></i>
+            </div>
+            <h4 class="title">SQL Server Maintenance</h4>
+            <div class="price">
+                  <p class="amount">$199<span>/ Per Visit</span></p>
+            </div>      
+      </div>
+      <!-- Table List -->
+      <ul class="table-list">
+            <li><i class="icofont icofont-ui-check"></i>Lorem ipsum dolor sit</li>
+            <li><i class="icofont icofont-ui-check"></i>Cubitur sollicitudin fentum</li>
+            <li class="cross"><i class="icofont icofont-ui-close"></i>Nullam interdum enim</li>
+            <li class="cross"><i class="icofont icofont-ui-close"></i>Donec ultricies metus</li>
+            <li class="cross"><i class="icofont icofont-ui-close"></i>Pellentesque eget nibh</li>
+      </ul>
+      <div class="table-bottom">
+            <a class="btn default-button" href="#">Book Now</a>
+      </div>
+      <!-- Table Bottom -->
+</div>
+</div>
+<!-- End Single Table-->
+<!-- Single Table -->
+<div class="col-lg-4 col-md-12 col-12">
+<div class="single-table">
+      <!-- Table Head -->
+      <div class="table-head">
+            <div class="icon">
+                  <i class="icofont icofont-robot"></i>
+            </div>
+            <h4 class="title">AI Services</h4>
+            <div class="price">
+                  <p class="amount">$299<span>/ Per Visit</span></p>
+            </div>      
+      </div>
+      <!-- Table List -->
+      <ul class="table-list">
+            <li><i class="icofont icofont-ui-check"></i>Lorem ipsum dolor sit</li>
+            <li><i class="icofont icofont-ui-check"></i>Cubitur sollicitudin fentum</li>
+            <li><i class="icofont icofont-ui-check"></i>Nullam interdum enim</li>
+            <li class="cross"><i class="icofont icofont-ui-close"></i>Donec ultricies metus</li>
+            <li class="cross"><i class="icofont icofont-ui-close"></i>Pellentesque eget nibh</li>
+      </ul>
+      <div class="table-bottom">
+            <a class="btn default-button" href="#">Book Now</a>
+      </div>
+      <!-- Table Bottom -->
+</div>
+</div>
+<!-- End Single Table-->
+<!-- Single Table -->
+<div class="col-lg-4 col-md-12 col-12">
+<div class="single-table">
+      <!-- Table Head -->
+      <div class="table-head">
+            <div class="icon">
+                  <i class="icofont-data"></i>
+            </div>
+            <h4 class="title">Data Services</h4>
+            <div class="price">
+                  <p class="amount">$399<span>/ Per Visit</span></p>
+            </div>      
+      </div>
+      <!-- Table List -->
+      <ul class="table-list">
+            <li><i class="icofont icofont-ui-check"></i>Lorem ipsum dolor sit</li>
+            <li><i class="icofont icofont-ui-check"></i>Cubitur sollicitudin fentum</li>
+            <li><i class="icofont icofont-ui-check"></i>Nullam interdum enim</li>
+            <li><i class="icofont icofont-ui-check"></i>Donec ultricies metus</li>
+            <li><i class="icofont icofont-ui-check"></i>Pellentesque eget nibh</li>
+      </ul>
+      <div class="table-bottom">
+            <a class="btn default-button" href="#">Book Now</a>
+      </div>
+      <!-- Table Bottom -->
+</div>
+</div>
+<!-- End Single Table-->
+</div>      
+</div>      
+</section>  
+<!--/ End Pricing Table -->
+
+
+
+
+
+
+
+
+
+<!-- start of searchbar modal -->
+<div class="container mt-5">
+<!-- Button to Open the Modal -->
+
+<!-- The Modal -->
+<div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog modal-lg" role="document">
+       <div class="modal-content">
+          <div class="modal-header default-background">
+            <div class="serach-container col-md-12 col-lg-12">
+                  <i class="fa fa-search icon"></i>
+                  <input  name="name" type="text" placeholder="Search here..."> 
+            </div>
+             
+              <button type="button" class="close text-white" data-dismiss="modal" aria-label="Close">
+                  <span aria-hidden="true">&times;</span>
+              </button>
+            </div>
+            <div class="modal-body search-results-section">
+               <p>Search results will appear here....</p>
+            </div>
+            
+        </div>
+    </div>
+</div>
+</div>
+
+<!-- end of search bar modal -->
+
+
+<!-- chatbot section -->
+<!-- Footer Area -->
+<footer id="footer" class="footer ">
+<!-- Footer Top -->
+<div class="footer-top">
+<div class="container">
+<div class="row">
+<div class="col-lg-2 col-md-6 col-12">
+      <div class="single-footer">
+            <h2>armely</h2>
+            
+      </div>
+</div>
+<div class="col-lg-2 col-md-6 col-12">
+      <div class="single-footer f-link">
+            <h2>About</h2>
+            <div class="row">
+                  <div class="col-lg-12 col-md-12 col-12">
+                        <ul>
+                              <li><a href="#"><i class="fa fa-caret-right" aria-hidden="true"></i>Case Studies</a></li>
+                              <li><a href="#"><i class="fa fa-caret-right" aria-hidden="true"></i>Job Board</a></li>
+                              <li><a href="#"><i class="fa fa-caret-right" aria-hidden="true"></i>Company Overview</a></li>
+                              <li><a href="#"><i class="fa fa-caret-right" aria-hidden="true"></i>Blog Articles </a></li>     
+                        </ul>
+                  </div>
+            </div>
+      </div>
+</div>
+<div class="col-lg-2 col-md-6 col-12">
+      <div class="single-footer f-link">
+            <h2>Services</h2>
+            <div class="row">
+                  <div class="col-lg-12 col-md-12 col-12">
+                        <ul>
+                              <li><a href="#"><i class="fa fa-caret-right" aria-hidden="true"></i>Data Services</a></li>
+                              <li><a href="#"><i class="fa fa-caret-right" aria-hidden="true"></i>Advisory Services</a></li>
+                              <li><a href="#"><i class="fa fa-caret-right" aria-hidden="true"></i>Managed Services</a></li>
+                              <li><a href="#"><i class="fa fa-caret-right" aria-hidden="true"></i>Artificial intelligence</a></li>  
+                        </ul>
+                  </div>
+            </div>
+      </div>
+</div>
+<div class="col-lg-3 col-md-6 col-12">
+      <div class="single-footer f-link">
+            <h2>Contact Us</h2>
+            <ul>
+                  <li><a href="#"><i class="fa fa-phone" aria-hidden="true"></i> +1 972 460 0643</a></li>
+                  <li><a href="#"><i class="fa fa-map-marker" aria-hidden="true"></i>2831 Eldorado Pkwy Suite 103-128 Frisco TX 75033</a></li>
+                  <li><a href="#"><i class="fa fa-envelope" aria-hidden="true"></i>info@armely.com</a></li>
+            </ul>
+      </div>
+</div>
+<div class="col-lg-3 col-md-6 col-12">
+      <div class="single-footer">
+            <h2>Follow Us</h2>
+            <ul class="social">
+                  <li><a href="#"><i class="icofont-linkedin"></i></a></li>
+                  <li><a href="#"><i class="icofont-github"></i></a></li>
+                  <li><a href="#"><i class="icofont-twitter"></i></a></li>
+                  <li><a href="#"><i class="icofont-youtube"></i></a></li>
+                  <li><a href="#"><i class="icofont-instagram"></i></a></li>
+            </ul>
+      </div>
+</div>
+</div>
+</div>
+</div>
+<!--/ End Footer Top -->
+<!-- Copyright -->
+<div class="copyright">
+<div class="container">
+<div class="row">
+<div class="col-lg-12 col-md-12 col-12">
+      <div class="copyright-content">
+            <p>&copy; 2024 ARMELY LLC., ALL RIGHTS RESERVED</p>
+      </div>
+</div>
+</div>
+</div>
+</div>
+<!--/ End Copyright -->
+</footer>
+<!--/ End Footer Area -->
+
+<!-- jquery Min JS -->
+<script src="js/jquery.min.js"></script>
+<!-- jquery Migrate JS -->
+<script src="js/jquery-migrate-3.0.0.js"></script>
+<!-- jquery Ui JS -->
+<script src="js/jquery-ui.min.js"></script>
+<!-- Easing JS -->
+<script src="js/easing.js"></script>
+<!-- Color JS -->
+<script src="js/colors.js"></script>
+<!-- Popper JS -->
+<script src="js/popper.min.js"></script>
+<!-- Bootstrap Datepicker JS -->
+<script src="js/bootstrap-datepicker.js"></script>
+<!-- Jquery Nav JS -->
+<script src="js/jquery.nav.js"></script>
+<!-- Slicknav JS -->
+<script src="js/slicknav.min.js"></script>
+<!-- ScrollUp JS -->
+<script src="js/jquery.scrollUp.min.js"></script>
+<!-- Niceselect JS -->
+<script src="js/niceselect.js"></script>
+<!-- Tilt Jquery JS -->
+<script src="js/tilt.jquery.min.js"></script>
+<!-- Owl Carousel JS -->
+<script src="js/owl-carousel.js"></script>
+<!-- counterup JS -->
+<script src="js/jquery.counterup.min.js"></script>
+<!-- Steller JS -->
+<script src="js/steller.js"></script>
+<!-- Wow JS -->
+<script src="js/wow.min.js"></script>
+<!-- Magnific Popup JS -->
+<script src="js/jquery.magnific-popup.min.js"></script>
+<!-- Counter Up CDN JS -->
+<script src="http://cdnjs.cloudflare.com/ajax/libs/waypoints/2.0.3/waypoints.min.js"></script>
+<!-- Bootstrap JS -->
+<script src="js/bootstrap.min.js"></script>
+<!-- Main JS -->
+<script src="js/main.js"></script>
+
+
+</body>
+</html>
