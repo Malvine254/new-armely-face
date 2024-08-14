e3b667e4fdd22c71f305fd1f9717b3bc86f941da
melo254 <owuormalvine75@gmail.com>
2024-08-14 22:44:00 +0300

    svg

--- company.php
+++ company.php
@@ -241,15 +241,17 @@
 <div class=" ">
 <center>
   <div class="row">
-<div class="col-md-4">
-  <img width="250" height="250"  class="img-fluid" src="images/affiliation/mbe.svg">
-</div>
-<div class="col-md-4">
-  <img width="230" height="230"  class="img-fluid" src="images/affiliation/affliation.png">
-</div>
-<div class="col-md-4">
-  <img width="250" height="250" class="img-fluid" src="images/affiliation/smb.svg">
-</div>
+    <div class="col-md-2"></div>
+    <div class="col-md-3">
+      <img width="250" height="250"  class="img-fluid" src="images/affiliation/mbe.svg">
+    </div>
+    <div class="col-md-3">
+      <img width="250" height="250" class="img-fluid" src="images/affiliation/smb.svg">
+    </div>
+    <div class="col-md-3 ">
+      <img width="200" height="200"  class="img-fluid mt-5" src="images/affiliation/affliation1.png">
+    </div>
+    <div class="col-md-2"></div>
 </center>
 </div>
 </div>
