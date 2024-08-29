48e5d1b0d3b0d3b5c0bcad12cfc5e6ac77f0c73b
melo254 <owuormalvine75@gmail.com>
2024-08-29 19:54:32 +0300

    moree

--- php/offers.php
+++ php/offers.php
@@ -27,8 +27,11 @@ if (isset($_POST['fname']) && isset($_POST['lname']) && isset($_POST['email2']) 
 
             // Execute the query
             if ($stmt->execute()) {
-                sendEmail($email, $fname." ".$lname, $phone,$country, $category);
-                sendDownloadLink($email, $category,$fname." ". $lname);
+                if ( sendEmail($email, $fname." ".$lname, $phone,$country, $category)==="1") {
+                   sendDownloadLink($email, $category,$fname." ". $lname);
+                }
+               ;
+                
             } else {
                 echo "Failed to submit the form";
             }
