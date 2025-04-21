95bc82154795e7fbf611eebe77231624d18598d9
melo254 <owuormalvine75@gmail.com>
2025-04-21 17:48:33 +0300

    all quote

--- test.php
+++ test.php
@@ -1,163 +1,36 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title></title>
-    <link rel="stylesheet" type="text/css" href="bootstrap.min.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/simple-line-icons/2.5.5/css/simple-line-icons.min.css">
+<?php
+// Load PHPMailer classes manually
+require 'phpMailer/src/PHPMailer.php';
+require 'phpMailer/src/SMTP.php';
+require 'phpMailer/src/Exception.php';
 
-</head>
-<body>
-    <style>
-        @import url(//fonts.googleapis.com/css?family=Montserrat:300,500);
-        .team4 {
-          font-family: "Montserrat", sans-serif;
-            color: #8d97ad;
-          font-weight: 300;
-        }
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
 
-        .team4 h1, .team4 h2, .team4 h3, .team4 h4, .team4 h5, .team4 h6 {
-          color: #3e4555;
-        }
 
-        .team4 .font-weight-medium {
-            font-weight: 500;
-        }
+$mail = new PHPMailer(true);
 
-        .team4 h5 {
-            line-height: 22px;
-            font-size: 18px;
-        }
+try {
+    // Server settings
+    $mail->isSMTP();
+    $mail->Host       = 'smtp.secureserver.net'; // GoDaddy SMTP server
+    $mail->SMTPAuth   = true;
+    $mail->Username   = 'ai.solutions@armely.com'; // your email
+    $mail->Password   = '@armely!234!2';      // replace with your real password
+    $mail->SMTPSecure = 'tls'; // or 'ssl' depending on GoDaddy config
+    $mail->Port       = 587;   // 465 for SSL
 
-        .team4 .subtitle {
-            color: #8d97ad;
-            line-height: 24px;
-                font-size: 13px;
-        }
+    // Recipients
+    $mail->setFrom('ai.solutions@armely.com', 'Armely AI Solutions');
+    $mail->addAddress('recipient@example.com', 'Recipient Name'); // Replace with recipient
 
-        .team4 ul li a {
-          color: #8d97ad;
-          padding-right: 15px;
-          -webkit-transition: 0.1s ease-in;
-          -o-transition: 0.1s ease-in;
-          transition: 0.1s ease-in;
-        }
+    // Content
+    $mail->isHTML(true);
+    $mail->Subject = 'Test Email from Armely';
+    $mail->Body    = 'This is a <b>test email</b> sent using PHPMailer and GoDaddy SMTP.';
 
-        .team4 ul li a:hover {
-          -webkit-transform: translate3d(0px, -5px, 0px);
-          transform: translate3d(0px, -5px, 0px);
-            color: #316ce8;
-        }
-    </style>
-    <div class="py-5 team4">
-  <div class="container">
-    <div class="row justify-content-center mb-4">
-      <div class="col-md-7 text-center">
-        <h3 class="mb-3">Experienced & Professional Team</h3>
-        <h6 class="subtitle">You can relay on our amazing features list and also our customer services will be great experience for you without doubt and in no-time</h6>
-      </div>
-    </div>
-    <div class="row">
-      <!-- column  -->
-      <div class="col-lg-3 mb-4">
-        <!-- Row -->
-        <div class="row">
-          <div class="col-md-12">
-            <img src="https://www.wrappixel.com/demos/ui-kit/wrapkit/assets/images/team/t1.jpg" alt="wrapkit" class="img-fluid rounded-circle" />
-          </div>
-          <div class="col-md-12 text-center">
-            <div class="pt-2">
-              <h5 class="mt-4 font-weight-medium mb-0">Michael Doe</h5>
-              <h6 class="subtitle mb-3">Property Specialist</h6>
-              <p>You can relay on our amazing features list and also our customer services will be great experience.</p>
-              <ul class="list-inline">
-                <li class="list-inline-item"><a href="#" class="text-decoration-none d-block px-1"><i class="icon-social-facebook"></i></a></li>
-                <li class="list-inline-item"><a href="#" class="text-decoration-none d-block px-1"><i class="icon-social-twitter"></i></a></li>
-                <li class="list-inline-item"><a href="#" class="text-decoration-none d-block px-1"><i class="icon-social-instagram"></i></a></li>
-                <li class="list-inline-item"><a href="#" class="text-decoration-none d-block px-1"><i class="icon-social-behance"></i></a></li>
-              </ul>
-            </div>
-          </div>
-        </div>
-        <!-- Row -->
-      </div>
-      <!-- column  -->
-      <!-- column  -->
-      <div class="col-lg-3 mb-4">
-        <!-- Row -->
-        <div class="row">
-          <div class="col-md-12">
-            <img src="https://www.wrappixel.com/demos/ui-kit/wrapkit/assets/images/team/t2.jpg" alt="wrapkit" class="img-fluid rounded-circle" />
-          </div>
-          <div class="col-md-12 text-center">
-            <div class="pt-2">
-              <h5 class="mt-4 font-weight-medium mb-0">Michael Doe</h5>
-              <h6 class="subtitle mb-3">Property Specialist</h6>
-              <p>You can relay on our amazing features list and also our customer services will be great experience.</p>
-              <ul class="list-inline">
-                <li class="list-inline-item"><a href="#" class="text-decoration-none d-block px-1"><i class="icon-social-facebook"></i></a></li>
-                <li class="list-inline-item"><a href="#" class="text-decoration-none d-block px-1"><i class="icon-social-twitter"></i></a></li>
-                <li class="list-inline-item"><a href="#" class="text-decoration-none d-block px-1"><i class="icon-social-instagram"></i></a></li>
-                <li class="list-inline-item"><a href="#" class="text-decoration-none d-block px-1"><i class="icon-social-behance"></i></a></li>
-              </ul>
-            </div>
-          </div>
-        </div>
-        <!-- Row -->
-      </div>
-      <!-- column  -->
-      <!-- column  -->
-      <div class="col-lg-3 mb-4">
-        <!-- Row -->
-        <div class="row">
-          <div class="col-md-12">
-            <img src="https://www.wrappixel.com/demos/ui-kit/wrapkit/assets/images/team/t3.jpg" alt="wrapkit" class="img-fluid rounded-circle" />
-          </div>
-          <div class="col-md-12 text-center">
-            <div class="pt-2">
-              <h5 class="mt-4 font-weight-medium mb-0">Michael Doe</h5>
-              <h6 class="subtitle mb-3">Property Specialist</h6>
-              <p>You can relay on our amazing features list and also our customer services will be great experience.</p>
-              <ul class="list-inline">
-                <li class="list-inline-item"><a href="#" class="text-decoration-none d-block px-1"><i class="icon-social-facebook"></i></a></li>
-                <li class="list-inline-item"><a href="#" class="text-decoration-none d-block px-1"><i class="icon-social-twitter"></i></a></li>
-                <li class="list-inline-item"><a href="#" class="text-decoration-none d-block px-1"><i class="icon-social-instagram"></i></a></li>
-                <li class="list-inline-item"><a href="#" class="text-decoration-none d-block px-1"><i class="icon-social-behance"></i></a></li>
-              </ul>
-            </div>
-          </div>
-        </div>
-        <!-- Row -->
-      </div>
-      <!-- column  -->
-      <!-- column  -->
-      <div class="col-lg-3 mb-4">
-        <!-- Row -->
-        <div class="row">
-          <div class="col-md-12">
-            <img src="https://www.wrappixel.com/demos/ui-kit/wrapkit/assets/images/team/t4.jpg" alt="wrapkit" class="img-fluid rounded-circle" />
-          </div>
-          <div class="col-md-12 text-center">
-            <div class="pt-2">
-              <h5 class="mt-4 font-weight-medium mb-0">Michael Doe</h5>
-              <h6 class="subtitle mb-3">Property Specialist</h6>
-              <p>You can relay on our amazing features list and also our customer services will be great experience.</p>
-              <ul class="list-inline">
-                <li class="list-inline-item"><a href="#" class="text-decoration-none d-block px-1"><i class="icon-social-facebook"></i></a></li>
-                <li class="list-inline-item"><a href="#" class="text-decoration-none d-block px-1"><i class="icon-social-twitter"></i></a></li>
-                <li class="list-inline-item"><a href="#" class="text-decoration-none d-block px-1"><i class="icon-social-instagram"></i></a></li>
-                <li class="list-inline-item"><a href="#" class="text-decoration-none d-block px-1"><i class="icon-social-behance"></i></a></li>
-              </ul>
-            </div>
-          </div>
-        </div>
-        <!-- Row -->
-      </div>
-    </div>
-  </div>
-</div>
-
-</body>
-</html>
-
+    $mail->send();
+    echo '✅ Message has been sent';
+} catch (Exception $e) {
+    echo "❌ Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+}
