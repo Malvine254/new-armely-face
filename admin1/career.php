097eebcb10f23ddb937f7282d1719f96cefb16d0
melo254 <owuormalvine75@gmail.com>
2025-06-03 09:35:06 -0500

    Your commit message here

--- admin1/career.php
+++ admin1/career.php
@@ -33,8 +33,7 @@
     
 </div>
 
- <div class="tab-content " id="ex-with-icons-content ">
-  <div class="tab-pane fade show active" id="ex-with-icons-tabs-1" role="tabpanel" aria-labelledby="ex-with-icons-tab-1">
+  <div class="tab-content content-area">
     <div class="mb-3">
       <form class="form-inline my-2 my-lg-0">
         <div class="input-group col" style="width: 350px;">
@@ -81,7 +80,6 @@
 
   <div class="tab-pane fade" id="ex-with-icons-tabs-2" role="tabpanel" aria-labelledby="ex-with-icons-tab-2" style="height: 60vh; overflow: scroll;">
     <table class="table table-bordered">
-        <div  class="table-responsive">
           <tr>
           <th>#</th>
           <th>Job Title</th>
@@ -92,13 +90,12 @@
           <th>Actions</th>
         </tr>
           <?php displayJobPosted(); ?>
-        </div>
+      
         
       </table>
   </div>
   <div class="tab-pane fade" id="ex-with-icons-tabs-3" role="tabpanel" aria-labelledby="ex-with-icons-tab-3"  style="height: 60vh; overflow: scroll;">
   <table class="table table-bordered myTable">
-    <div  class="table-responsive">
       <tr>
         <th>#</th>
         <th>Job Title</th>
@@ -112,14 +109,12 @@
       </tr > 
       <?php displayShortlistedCandidates(); ?>
   
-    </div>
     
   </table>
 </div>
 
 <div class="tab-pane fade" id="ex-with-icons-tabs-4" role="tabpanel" aria-labelledby="ex-with-icons-tab-4"  style="height: 60vh; overflow: scroll;">
     <table class="table table-bordered myTable">
-      <div  class="table-responsive">
         <tr>
           <th>#</th>
           <th>Full Name</th>
@@ -129,9 +124,6 @@
           <th>Gender</th>
           <th>Actions</th>
         </tr > 
-     
-      
-      </div>
       
     </table>
 </div>
