702027b86bb37561474a90d7b9875620b8366bf7
melo254 <owuormalvine75@gmail.com>
2025-08-29 14:25:38 -0500

    more

--- company.php
+++ company.php
@@ -90,7 +90,7 @@
       <img width="250" height="250" class="img-fluid" src="images/affiliation/smb.svg">
     </div>
      <div class="col-md-3">
-      <img   class="img-fluid mt-4" src="images/affiliation/solution_partner.jpg">
+      <img   class="img-fluid mt-4" src="images/affiliation/partner.png">
     </div>
     <div class="col-md-3 ">
       <img width="200" height="200"  class="img-fluid mt-5" src="images/affiliation/affliation1.png">
