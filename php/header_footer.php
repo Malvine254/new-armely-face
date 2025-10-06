bd4c93aa716043d9c7b34a7aa7996b8628f7941e
melo254 <owuormalvine75@gmail.com>
2025-10-06 09:54:34 -0500

    Intermediate commit message changed

--- php/header_footer.php
+++ php/header_footer.php
@@ -76,7 +76,7 @@ function getHeader($pageName) {
 	    📢 <!--<b>Estimate your Fabric capacity needs!</b> Check out Microsoft Fabric SKU Estimator. <a href="service-details?name=fabric_capacity">Learn More</a>--->
 		    <b>Do you need AI Proof of Concept (PoC) Starter Pack ?</b> Request your AI Proof of Concept Starter Pack Today. <a href="service-details?name=pocstarter-ai">Learn More</a>
 		    <span class="close-btn" onclick="closeBanner()">&times;</span>
-	</div>-->
+	</div> -->
 	
 		<section>	
 		<!-- Floating Action Button -->
